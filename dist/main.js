6487c94542f59d5ca1f49b5ed3be0bdcf0444873
LuizFelipe-tech <81056246+LuizFelipe-tech@users.noreply.github.com>
2025-08-29 07:26:24 -0300

    Starting implementing dynamic text effect with word deletion and addition; update styles and HTML structure.

--- dist/main.js
+++ dist/main.js
@@ -1,41 +1,59 @@
-class ProximityEffect {
-    selector;
-    text;
-    elements;
-    mousePosition = { x: 0, y: 0 };
-    constructor(selector, text) {
-        this.selector = selector;
-        this.text = text;
-        this.createElements();
-        this.elements = document.querySelectorAll(`${this.selector} span`);
-    }
-    createElements() {
-        const container = document.querySelector(this.selector);
-        if (container) {
-            const charSpans = this.text
-                .split('')
-                .map(char => `<span class = "char-element">${char}</span>`)
-                .join('');
-            container.innerHTML = charSpans;
+document.addEventListener('DOMContentLoaded', () => {
+    class DynamicTextEffect {
+        dynamicText;
+        wordsCount;
+        exhibitedWord;
+        constructor() {
+            this.dynamicText = document.querySelector('#dynamic-text');
+            this.wordsCount = ['Soluções.', 'Futuro.', 'Sistemas.', 'Código.', 'Inovação.', 'Resultados.'];
+            this.exhibitedWord = 0;
+        }
+        DeleteWord() {
+            let count = 0;
+            console.log('1');
+            let deleteCharacterAt = this.dynamicText.innerText.length - 1;
+            console.log('2');
+            console.log(this.dynamicText.innerHTML);
+            var intervalID = setInterval(() => {
+                console.log('3');
+                count++;
+                this.dynamicText.innerHTML = this.dynamicText.innerHTML.slice(0, deleteCharacterAt);
+                console.log(this.dynamicText.innerText.slice(0, deleteCharacterAt));
+                console.log(this.dynamicText.innerHTML);
+                console.log('4');
+                deleteCharacterAt--;
+                if (this.wordsCount[this.exhibitedWord].length == 0) {
+                    console.log('5');
+                    clearInterval(intervalID);
+                }
+            }, 10000);
+            return;
+        }
+        NewWord() {
+            if (this.exhibitedWord > 5) {
+                this.exhibitedWord = 0;
+            }
+            else {
+                this.exhibitedWord++;
+            }
+            let characterIndex = this.wordsCount[this.exhibitedWord].length - 1;
+            let count = 0;
+            let intervalID = setInterval(() => {
+                this.dynamicText.innerText = this.dynamicText.innerText.slice(0, characterIndex);
+                if (count < this.wordsCount[this.exhibitedWord].length) {
+                    count++;
+                }
+                else {
+                    clearInterval(intervalID);
+                }
+            }, 100);
         }
     }
-    setupListeners() {
-        window.addEventListener('mousemove', (event) => {
-            this.mousePosition.x = event.clientX;
-            this.mousePosition.y = event.clientY;
-        });
-    }
-    updateStyles() {
-        this.elements.forEach(element => {
-            const rect = element.getBoundingClientRect();
-            const centerX = rect.left + rect.width / 2;
-            const centerY = rect.top + rect.height / 2;
-            const deltaX = this.mousePosition.x - centerX;
-            const deltaY = rect.top + rect.height / 2;
-            const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-            const percentageDistance = distance;
-        });
-    }
-}
+    const heroTextEffect = new DynamicTextEffect();
+    console.log('0');
+    console.log(heroTextEffect.dynamicText.innerHTML);
+    heroTextEffect.DeleteWord();
+    heroTextEffect.NewWord();
+});
 export {};
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,78 +1,57 @@
-    interface MouseCoordinates{
-    x: number;
-    y: number
-}
-class ProximityEffect{
-  private readonly selector: string;
-  private readonly text: string;
-
-  private elements: NodeListOf<HTMLSpanElement>;
-  private mousePosition: MouseCoordinates = { x: 0, y: 0 };
-    constructor(selector: string, text: string){
-        this.selector = selector
-        this.text = text
-        this.createElements()
-        this.elements = document.querySelectorAll<HTMLSpanElement>(`${this.selector} span`)
-    }
-    private createElements(): void{
-        const container = document.querySelector(this.selector)
-        if(container){
-            const charSpans = this.text
-            .split('')
-            .map(char => `<span class = "char-element">${char}</span>`)
-            .join('')
-            container.innerHTML = charSpans
+document.addEventListener('DOMContentLoaded', () => {
+    class DynamicTextEffect{
+        public dynamicText:HTMLParagraphElement
+        private wordsCount: string[]
+        private exhibitedWord: number
+        constructor(){
+            this.dynamicText = document.querySelector('#dynamic-text')!
+            this.wordsCount = ['Soluções.', 'Futuro.', 'Sistemas.', 'Código.', 'Inovação.', 'Resultados.']
+            this.exhibitedWord = 0
         }
+        DeleteWord(): void{
+            let count: number = 0
+            console.log('1')
+            let deleteCharacterAt: number = this.dynamicText.innerText.length - 1
+            console.log('2')
+            console.log(this.dynamicText.innerHTML)
+            var intervalID = setInterval(()=>{
+                console.log('3')
+                count++
+                this.dynamicText.innerHTML = this.dynamicText.innerHTML.slice(0, deleteCharacterAt)
+                console.log(this.dynamicText.innerText.slice(0, deleteCharacterAt))
+                console.log(this.dynamicText.innerHTML)
+                console.log('4')
+                deleteCharacterAt--
+                if(this.wordsCount[this.exhibitedWord]!.length == 0 ){
+                    console.log('5')
+                    clearInterval(intervalID)
+                }
+            }, 10000)
+
+            return
+        }
+        NewWord(): void{
+            if(this.exhibitedWord > 5){
+                this.exhibitedWord = 0
+            }else{
+                this.exhibitedWord++  
+            }
+            let characterIndex: number = this.wordsCount[this.exhibitedWord]!.length - 1
+                let count: number = 0
+                let intervalID = setInterval(()=>{
+                        this.dynamicText.innerText = this.dynamicText.innerText.slice(0, characterIndex)
+                        if(count < this.wordsCount[this.exhibitedWord]!.length){
+                            count++
+                        }else{
+                            clearInterval(intervalID)
+                        }
+                    }, 100)   
+        }
+        
     }
-    private setupListeners(): void{
-        window.addEventListener('mousemove', (event) => {
-            this.mousePosition.x = event.clientX
-            this.mousePosition.y = event.clientY
-        })
-    }
-    private updateStyles(): void{
-        this.elements.forEach(element => {
-            const rect = element.getBoundingClientRect()
-            const centerX = rect.left + rect.width / 2
-            const centerY = rect.top + rect.height /2
-            const deltaX = this.mousePosition.x - centerX
-            const deltaY = rect.top + rect.height / 2
-            const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-
-            const percentageDistance = distance;
-        })
-    }
-    /*private updateStyles(): void {
-    // 1. Itera sobre cada elemento <span> que a gente selecionou no construtor.
-    this.elements.forEach(element => {
-        // 2. Obtém a posição e as dimensões exatas do <span> na tela.
-        const rect = element.getBoundingClientRect();
-        
-        // 3. Calcula as coordenadas do centro do <span>.
-        const centerX = rect.left + rect.width / 2;
-        const centerY = rect.top + rect.height / 2;
-        
-        // 4. Calcula a distância euclidiana entre o mouse e o centro do <span>.
-        const deltaX = this.mousePosition.x - centerX;
-        const deltaY = this.mousePosition.y - centerY;
-        const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-        
-        // 5. Mapeia a distância para os valores de desfoque e brilho.
-        // O desfoque diminui conforme a distância diminui.
-        const blurValue = Math.min(distance / 20, 20); 
-        // O brilho aumenta conforme a distância diminui.
-        const glowValue = Math.max(100 - distance / 1.5, 0); 
-        
-        // 6. Atualiza as variáveis CSS para cada elemento.
-        element.style.setProperty('--distance', `${blurValue}px`);
-        element.style.setProperty('--glow', `${glowValue}`);
-    });
-
-    // 7. Pede ao navegador para chamar este método novamente no próximo quadro.
-    requestAnimationFrame(() => this.updateStyles());
-}*/
-}
-export{}
-
-
-
+    const heroTextEffect: DynamicTextEffect = new DynamicTextEffect()
+    console.log('0')
+    console.log(heroTextEffect.dynamicText.innerHTML)
+    heroTextEffect.DeleteWord()
+    heroTextEffect.NewWord()
+})
