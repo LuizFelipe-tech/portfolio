6400ebd08bce6766eeeef488822e66ce3449bff5
Luiz Felipe Nascimento Melos <luizfelipemelos@gmail.com>
2025-08-29 12:26:38 -0300

    Refactor dynamic text effect implementation; remove unused methods and variables

--- dist/main.js
+++ dist/main.js
@@ -1,59 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
     class DynamicTextEffect {
         dynamicText;
-        wordsCount;
-        exhibitedWord;
+        isIntervalRunning = false;
+        //private wordsCount: string[]
+        // private exhibitedWord: number
         constructor() {
             this.dynamicText = document.querySelector('#dynamic-text');
-            this.wordsCount = ['Soluções.', 'Futuro.', 'Sistemas.', 'Código.', 'Inovação.', 'Resultados.'];
-            this.exhibitedWord = 0;
+            //this.wordsCount = ['Soluções.', 'Futuro.', 'Sistemas.', 'Código.', 'Inovação.', 'Resultados.']
+            //this.exhibitedWord = 0
         }
-        DeleteWord() {
-            let count = 0;
-            console.log('1');
-            let deleteCharacterAt = this.dynamicText.innerText.length - 1;
-            console.log('2');
-            console.log(this.dynamicText.innerHTML);
-            var intervalID = setInterval(() => {
-                console.log('3');
-                count++;
-                this.dynamicText.innerHTML = this.dynamicText.innerHTML.slice(0, deleteCharacterAt);
-                console.log(this.dynamicText.innerText.slice(0, deleteCharacterAt));
-                console.log(this.dynamicText.innerHTML);
-                console.log('4');
-                deleteCharacterAt--;
-                if (this.wordsCount[this.exhibitedWord].length == 0) {
-                    console.log('5');
-                    clearInterval(intervalID);
-                }
-            }, 10000);
-            return;
-        }
-        NewWord() {
-            if (this.exhibitedWord > 5) {
-                this.exhibitedWord = 0;
-            }
-            else {
-                this.exhibitedWord++;
-            }
-            let characterIndex = this.wordsCount[this.exhibitedWord].length - 1;
-            let count = 0;
+        eraseTextGradually() {
+            let intervalCount = 0;
+            let deleteCharIndex = this.dynamicText.innerText.length - 1;
             let intervalID = setInterval(() => {
-                this.dynamicText.innerText = this.dynamicText.innerText.slice(0, characterIndex);
-                if (count < this.wordsCount[this.exhibitedWord].length) {
-                    count++;
-                }
-                else {
+                this.isIntervalRunning = true;
+                intervalCount++;
+                this.dynamicText.innerHTML = this.dynamicText.innerHTML.slice(0, deleteCharIndex);
+                deleteCharIndex--;
+                if (this.dynamicText.innerText.length === 0) {
+                    this.isIntervalRunning = false;
                     clearInterval(intervalID);
                 }
             }, 100);
+            return;
         }
     }
     const heroTextEffect = new DynamicTextEffect();
-    console.log('0');
-    console.log(heroTextEffect.dynamicText.innerHTML);
-    heroTextEffect.DeleteWord();
-    heroTextEffect.NewWord();
+    setTimeout(() => {
+        heroTextEffect.eraseTextGradually();
+    }, 1500);
+    //heroTextEffect.NewWord()
 });
 export {};
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
     class DynamicTextEffect{
-        public dynamicText:HTMLParagraphElement
-        private wordsCount: string[]
-        private exhibitedWord: number
+        private dynamicText:HTMLParagraphElement
+        public isIntervalRunning: boolean = false
+        //private wordsCount: string[]
+       // private exhibitedWord: number
         constructor(){
             this.dynamicText = document.querySelector('#dynamic-text')!
-            this.wordsCount = ['Soluções.', 'Futuro.', 'Sistemas.', 'Código.', 'Inovação.', 'Resultados.']
-            this.exhibitedWord = 0
+            //this.wordsCount = ['Soluções.', 'Futuro.', 'Sistemas.', 'Código.', 'Inovação.', 'Resultados.']
+            //this.exhibitedWord = 0
         }
-        DeleteWord(): void{
-            let count: number = 0
-            console.log('1')
-            let deleteCharacterAt: number = this.dynamicText.innerText.length - 1
-            console.log('2')
-            console.log(this.dynamicText.innerHTML)
-            var intervalID = setInterval(()=>{
-                console.log('3')
-                count++
-                this.dynamicText.innerHTML = this.dynamicText.innerHTML.slice(0, deleteCharacterAt)
-                console.log(this.dynamicText.innerText.slice(0, deleteCharacterAt))
-                console.log(this.dynamicText.innerHTML)
-                console.log('4')
-                deleteCharacterAt--
-                if(this.wordsCount[this.exhibitedWord]!.length == 0 ){
-                    console.log('5')
-                    clearInterval(intervalID)
+        eraseTextGradually(): void{
+            let intervalCount: number = 0
+            let deleteCharIndex: number = this.dynamicText.innerText.length - 1
+            let intervalID = setInterval(()=>{
+                this.isIntervalRunning = true
+                intervalCount++
+                this.dynamicText.innerHTML = this.dynamicText.innerHTML.slice(0, deleteCharIndex)
+                deleteCharIndex--
+                if(this.dynamicText.innerText!.length === 0 ){
+                    this.isIntervalRunning = false
+                    clearInterval(intervalID)                           
                 }
-            }, 10000)
+            }, 100)
 
             return
         }
-        NewWord(): void{
+        /*NewWord(): void{
             if(this.exhibitedWord > 5){
                 this.exhibitedWord = 0
             }else{
@@ -46,12 +41,13 @@ document.addEventListener('DOMContentLoaded', () => {
                             clearInterval(intervalID)
                         }
                     }, 100)   
-        }
+        }*/
         
     }
     const heroTextEffect: DynamicTextEffect = new DynamicTextEffect()
-    console.log('0')
-    console.log(heroTextEffect.dynamicText.innerHTML)
-    heroTextEffect.DeleteWord()
-    heroTextEffect.NewWord()
+    setTimeout(()=>{
+        heroTextEffect.eraseTextGradually()
+    }, 1500)
+
+    //heroTextEffect.NewWord()
 })
